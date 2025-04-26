2a67cd5e29791539b9e69040bcbdac74b78ed877
Nitesh Kumar Vashisht <nitesh@shooliniuniversity.com>
2025-04-26 17:05:03 +0530

    Update server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+let song = [
+  {
+    "filename": "Trrippen.mp3",
+    "url": "https://niteshkumarvashisht.github.io/musicserver/Trrippen.mp3"
+  },
+  {
+    "filename": "babyagain.mp3",
+    "url": "https://niteshkumarvashisht.github.io/musicserver/babyagain.mp3"
+  },
+  {
+    "filename": "leavemealone.mp3",
+    "url": "https://niteshkumarvashisht.github.io/musicserver/leavemealone.mp3"
+  },
+  {
+    "filename": "momentapart.mp3.mp3",
+    "url": "https://niteshkumarvashisht.github.io/musicserver/momentapart.mp3.mp3"
+  },
+  {
+    "filename": "tenthirtyfive.mp3",
+    "url": "https://niteshkumarvashisht.github.io/musicserver/tenthirtyfive.mp3"
+  }
+]
